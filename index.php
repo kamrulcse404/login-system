2658e8dfcb257a4b7e8c600656bafa2a0be4fefb
kamrulcse404 <kamrul.h01960@gmail.com>
2022-09-17 01:18:18 +0600

    initial add

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require_once __DIR__ . '/partials/header.php'; ?>
+
+
+<!-- navbar -->
+<?php require_once __DIR__ . '/partials/nav.php' ?>
+<!-- navbar end -->
+
+<!-- main -->
+<div class="container">
+    <div class="row">
+        <h1 class="text-center" style="color: #1c211d;">Welcme to our Login System</h1>
+        <p class="text-center">what you want? Login or SignUp</p>
+    </div>
+</div>
+<!-- main end -->
+
+<!-- footer -->
+<?php require_once __DIR__ . '/partials/footer.php'; ?>
---
+++ partials/nav.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light" style="background-color: #16261a;">
+    <div class="container">
+        <a class="navbar-brand text-light" style="color: #d8e8dc;" href="./">Login System</a>
+        <div>
+            <a href="login.php" class="btn btn-outline-primary d-left">Login</a>
+            <a href="signUp.php" class="btn btn-outline-danger d-left">SignUp</a>
+        </div>
+    </div>
+</nav>
